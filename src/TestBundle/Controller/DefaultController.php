48ba84fccb5c94158d73195419f0a39dda7021b5
abbeshamza <abb.hamza.2013@gmail.com>
2016-03-02 12:39:51 +0100

    jenkins

--- src/TestBundle/Controller/DefaultController.php
+++ src/TestBundle/Controller/DefaultController.php
@@ -18,6 +18,7 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+      var a ;
       //return array('hello' => 'world');
       echo"hamza";
       echo"abbes";
